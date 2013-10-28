688229c92d105a1dc7c4a002648bcb2693a95b4a
Yomguithereal <guillaumeplique@msn.com>
2013-10-28 11:19:06 +0100

    Laying playground

--- assets/js/mtgnode/playground/Controller.js
+++ assets/js/mtgnode/playground/Controller.js
@@ -11,7 +11,33 @@
 ;(function($, w, domino, CardTemplate, undefined){
   "use strict";
 
-  var id = $("#playground_id").attr('value');
+
+  // Controller
+  //============
+
+  var controller = new domino({
+    name: 'PlaygroundController',
+    properties: [],
+    services: [],
+    hacks: []
+  });
+
+
+  // Instanciating Widgets
+  $('#card_viewer_widget').cardViewerWidget({container: '#deck_builder_container', cards: '.card-min'});
+
+
+
+
+
+
+
+
+
+
+  // Debug
+
+  var id = $('#playground_id').attr('value');
   console.log(id);
 
   socket.get('/playground/connect/'+id, function(res){
--- assets/js/mtgnode/simple/Login.js
+++ assets/js/mtgnode/simple/Login.js
@@ -1,15 +1,15 @@
 // Simple login actions
 $(document).ready(function(){
 
-	// Override connect
-	$('.override-connect').click(function(){
-		$.post('ajax/login/authenticate', {username: $(this).attr('username'), password: $(this).attr('password')}, function(result){
-			if(!!result.authenticated){
-				location.href = '/lobby';
-			}
-			else{
-				alert('Wrong Login');
-			}
-		});
-	});
+  // Override connect
+  $('.override-connect').click(function(){
+    $.post('ajax/login/authenticate', {username: $(this).attr('username'), password: $(this).attr('password')}, function(result){
+      if(!!result.authenticated){
+        location.href = '/lobby';
+      }
+      else{
+        alert('Wrong Login');
+      }
+    });
+  });
 });
