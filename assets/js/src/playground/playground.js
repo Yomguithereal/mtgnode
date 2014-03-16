26f50171533929094c0da736e8c1917deefc0ded
Yomguithereal <guillaumeplique@gmail.com>
2014-03-16 13:29:47 +0100

    Building playground namespace

---
+++ assets/js/src/playground/playground.js
@@ -0,0 +1,17 @@
+(function(undefined) {
+  'use strict';
+
+  /**
+   * Playground Namespace
+   * =====================
+   *
+   * Register some global configuration.
+   */
+  var _config = {};
+
+  /**
+   * Exporting
+   * ----------
+   */
+  utilities.pkg('playground.globals', _config);
+}).call(this);
---
+++ assets/js/src/playground/playground.properties.js
@@ -0,0 +1,150 @@
+(function(undefined) {
+  'use strict';
+
+  /**
+   * Playground Domino Properties
+   * =============================
+   *
+   * Properties registry
+   */
+  var _properties = [
+
+      // Generic
+      {
+        id: 'gameId',
+        type: 'number|string',
+        value: 'debug'
+      },
+      {
+        id: 'debug',
+        type: 'boolean',
+        value: false
+      },
+
+      // Me
+      {
+        id: 'mySide',
+        type: 'number',
+        value: 0
+      },
+      {
+        id: 'myUser',
+        type: 'object',
+        value: {}
+      },
+      {
+        id: 'myHitpoints',
+        type: 'number',
+        value: 20,
+        dispatch: 'myHitpointsUpdated'
+      },
+      {
+        id: 'myInfection',
+        type: 'number',
+        value: 0,
+        dispatch: 'myInfectionUpdated'
+      },
+      {
+        id: 'myTurn',
+        type: 'boolean',
+        value: false,
+        dispatch: 'myTurnUpdated'
+      },
+      {
+        id: 'myDeck',
+        type: 'array',
+        value: [],
+        dispatch: 'myDeckUpdated'
+      },
+      {
+        id: 'myHand',
+        type: 'array',
+        value: [],
+        dispatch: 'myHandUpdated'
+      },
+      {
+        id: 'myBattlefield',
+        type: 'array',
+        value: [],
+        dispatch: 'myBattlefieldUpdated'
+      },
+      {
+        id: 'myGraveyard',
+        type: 'array',
+        value: [],
+        dispatch: 'myGraveyardUpdated'
+      },
+      {
+        id: 'myExile',
+        type: 'array',
+        value: [],
+        dispatch: 'myExileUpdated'
+      },
+
+      // Opponent
+      {
+        id: 'opSide',
+        type: 'number',
+        value: 0
+      },
+      {
+        id: 'opUser',
+        type: 'object',
+        value: {}
+      },
+      {
+        id: 'opHitpoints',
+        type: 'number',
+        value: 20,
+        dispatch: 'opHitpointsUpdated'
+      },
+      {
+        id: 'opInfection',
+        type: 'number',
+        value: 0,
+        dispatch: 'opInfectionUpdated'
+      },
+      {
+        id: 'opTurn',
+        type: 'boolean',
+        value: false,
+        dispatch: 'opTurnUpdated'
+      },
+      {
+        id: 'opDeck',
+        type: 'array',
+        value: [],
+        dispatch: 'opDeckUpdated'
+      },
+      {
+        id: 'opHand',
+        type: 'array',
+        value: [],
+        dispatch: 'opHandUpdated'
+      },
+      {
+        id: 'opBattlefield',
+        type: 'array',
+        value: [],
+        dispatch: 'opBattlefieldUpdated'
+      },
+      {
+        id: 'opGraveyard',
+        type: 'array',
+        value: [],
+        dispatch: 'opGraveyardUpdated'
+      },
+      {
+        id: 'opExile',
+        type: 'array',
+        value: [],
+        dispatch: 'opExileUpdated'
+      }
+    ];
+
+  /**
+   * Exporting
+   * ----------
+   */
+  utilities.pkg('playground.properties', _properties);
+}).call(this);
