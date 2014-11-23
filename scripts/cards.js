89fbaad985b99cf61d072ea13e502b56ac90fccf
Yomguithereal <guillaumeplique@gmail.com>
2014-11-23 21:46:50 +0100

    Better scripts

--- scripts/cards.js
+++ scripts/cards.js
@@ -6,20 +6,36 @@
  */
 var sets = require('../data/mtgjson.json'),
     fs = require('fs'),
-    helpers = require('./helpers.js'),
     _ = require('lodash'),
     cards = [];
 
+// Helpers
+function validSet(set) {
+  return (!~['ATH', 'ITP', 'DKM', 'RQS', 'DPA'].indexOf(set.code) &&
+          set.code.charAt(0) !== 'p');
+}
+
 // Looping over sets
 for (var set in sets) {
-  if (!helpers.validSet(sets[set]))
-    continue;
+  if (validSet(sets[set])) {
 
-  cards = cards.concat(sets[set].cards);
+    // Saving card
+    cards = cards.concat(sets[set].cards);
+
+    // Registering set
+    sets[set].nb_cards = sets[set].cards.length;
+    delete sets[set].cards;
+  }
+  else {
+
+    // Avoiding fishy set
+    delete sets[set];
+  }
 }
 
 // Reversing in order to get latest card first
 cards.reverse();
 
 // Outputting
+fs.writeFileSync(__dirname + '/../data/sets.json', JSON.stringify(sets));
 fs.writeFileSync(__dirname + '/../data/cards.json', JSON.stringify(cards));
