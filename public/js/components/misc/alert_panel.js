80ab64ed288e430fc5552191e0232eb275aca5cc
Yomguithereal <guillaumeplique@gmail.com>
2014-12-07 22:20:19 +0100

    Scaffolding alert component

---
+++ public/js/components/misc/alert_panel.js
@@ -0,0 +1,9 @@
+/**
+ * MTGNode Alert Panel
+ * ====================
+ *
+ * A component waiting for alert events to be dispatched and displaying
+ * feeback to inform the user.
+ */
+var React = require('react'),
+    controller = require('../../controllers/main.js');
